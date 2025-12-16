efafecb2f84c04785cbc69d1369cab757eda3752
WSewell-arch <Winston.sewell12@gmail.com>
2025-12-16 13:12:04 -0500

    Add files via upload

---
+++ 2025AU-Morning-Engines-2/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject PauseMenuUI;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+              Resume();  
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+ 
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game....");
+        Application.Quit();
+    }
+
+}
